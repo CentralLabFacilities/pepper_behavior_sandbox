b69d250c9da4e9ac8993723d2690d39afb0ec4df
Florian Lier <fl@techfak.uni-bielefeld.de>
2017-08-23 12:19:32 +0200

    version 2

--- smach/geniale_experiment.py
+++ smach/geniale_experiment.py
@@ -9,9 +9,12 @@ import actionlib
 from std_msgs.msg import String
 from math import radians, degrees
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from people_msgs.msg import Person, People
 from naoqi_bridge_msgs.msg import SpeechWithFeedbackAction, SpeechWithFeedbackActionGoal, SpeechWithFeedbackGoal
 from geometry_msgs.msg import PoseWithCovarianceStamped, Quaternion
+from visualization_msgs.msg import Marker, MarkerArray
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
+
 
 class DataAcutators:
     def __init__(self):
@@ -50,13 +53,34 @@ class DataAcutators:
 class DataSensors:
     def __init__(self):
         self.speech_rec_context = rospy.Subscriber("/pepper_robot/speechrec/context", String, self.context_callback)
+        self.people_sensor = rospy.Subscriber("/clf_detect_dlib_faces/people", People, self.people_callback)
+        self.speech_rec_context = rospy.Subscriber("/clf_detect_objects_surb/objects", MarkerArray,
+                                                   self.object_callback)
         self.current_context = ""
+        self.current_objects = []
+        self.current_people = []
 
     def reset_context(self):
         self.current_context = ""
 
+    def reset_objects(self):
+        self.current_objects = []
+
+    def reset_people(self):
+        self.current_people = []
+
     def context_callback(self, data):
         self.current_context = data.data.strip()
+
+    def object_callback(self, data):
+        self.reset_objects()
+        for m in data.markers:
+            self.current_objects.append(m.text)
+
+    def people_callback(self, data):
+        self.reset_people()
+        for p in data.people:
+            self.current_people.append(p.name)
 
 
 class WaitForCommand(smach.State):
@@ -76,6 +100,14 @@ class WaitForCommand(smach.State):
             rospy.loginfo('Go back to init position')
             self.ds.reset_context()
             return 'init'
+        elif self.ds.current_context == "What is on the table":
+            rospy.loginfo('What is on the table')
+            self.ds.reset_context()
+            return 'what'
+        elif self.ds.current_context == "What is the gender of the person":
+            rospy.loginfo('What is the gender of the person')
+            self.ds.reset_context()
+            return 'gender'
         rospy.logwarn('No valid command')
         self.ds.reset_context()
         return 'none'
@@ -89,12 +121,12 @@ class GoToTable(smach.State):
     def execute(self, userdata):
         rospy.loginfo('Entering State GoToTable')
         self.da.say_something("Okay my friend, I will drive to the table")
-        goal = self.da.set_nav_goal(5.98, -6.39, 0.0, 0.0, 0.99, -0.12)
+        goal = self.da.set_nav_goal(6.01, -6.244, 0.0, 0.0, 0.98, -0.159)
         if goal == "3":
             self.da.say_something("I am done. What shall I do?")
             return 'arrived'
         else:
-            self.da.say_something("I could not reach the table, oooooh no.")
+            self.da.say_something("I could not reach the table, oh no!")
             rospy.logwarn('Could not reach table')
             return 'fail'
 
@@ -117,6 +149,49 @@ class GoToInit(smach.State):
             return 'fail'
 
 
+class WhatIs(smach.State):
+    def __init__(self, _ds, _da):
+        smach.State.__init__(self, outcomes=['report'])
+        self.ds = _ds
+        self.da = _da
+
+    def execute(self, userdata):
+        rospy.loginfo('Entering State WhatIs')
+        see = "I see "
+        objects = self.ds.current_objects
+        if len(objects) < 1:
+            self.da.say_something("I can not see any objects")
+        else:
+            for o in objects:
+                self.da.say_something(see + str(o))
+        self.ds.reset_objects()
+        return 'report'
+
+
+class WhoIs(smach.State):
+    def __init__(self, _ds, _da):
+        smach.State.__init__(self, outcomes=['whois'])
+        self.ds = _ds
+        self.da = _da
+
+    def execute(self, userdata):
+        rospy.loginfo('Entering State WhatIs')
+        people = self.ds.current_people
+        if len(people) < 1:
+            self.da.say_something("I can not see any person, I am sorry.")
+        else:
+            many = "I see " + str(len(people))
+            self.da.say_something(many)
+            for o in people:
+                one = "One person is "
+                age = "The age is "
+                self.da.say_something(one + str(o).split(':')[0])
+                print str(o).split(':')[1]
+                self.da.say_something(age + str(o).split(':')[1])
+        self.ds.reset_people()
+        return 'whois'
+
+
 def main():
     rospy.init_node('geniale_pepper_state_machine')
     ds = DataSensors()
@@ -130,10 +205,11 @@ def main():
         smach.StateMachine.add('WAIT', WaitForCommand(ds),
                                transitions={'table': 'GOTOTABLE',
                                             'init': 'GOTOINIT',
-                                            'age': 'GOTOTABLE',
-                                            'what': 'GOTOTABLE',
-                                            'gender': 'GOTOTABLE',
+                                            'age': 'WAIT',
+                                            'what': 'WHATIS',
+                                            'gender': 'WHOIS',
                                             'none': 'WAIT'})
+
         smach.StateMachine.add('GOTOTABLE', GoToTable(da),
                                transitions={'arrived': 'WAIT',
                                             'fail': 'WAIT'})
@@ -141,6 +217,16 @@ def main():
         smach.StateMachine.add('GOTOINIT', GoToInit(da),
                                transitions={'arrived': 'WAIT',
                                             'fail': 'WAIT'})
+
+        smach.StateMachine.add('WHATIS', WhatIs(ds, da),
+                               transitions={'report': 'WAIT'})
+
+        smach.StateMachine.add('WHOIS', WhoIs(ds, da),
+                               transitions={'whois': 'WAIT'})
+
+    # Introspection viewer
+    sis = smach_ros.IntrospectionServer('server_name', sm, '/GENIALE')
+    sis.start()
 
     # Execute SMACH plan
     outcome = sm.execute()
