34e541e1592a7ded79a107b9bf489006fe97f7b7
Florian Lier <fl@techfak.uni-bielefeld.de>
2017-09-05 20:30:55 +0200

    sim to FALSE

--- pepper_behavior/skills/move_head.py
+++ pepper_behavior/skills/move_head.py
@@ -9,11 +9,12 @@ class MoveHeadPepper(smach.State):
         if _hv or _hh:
             input_k = []
         else:
-            input_k = ['head_vertical','head_horizontal']
+            input_k = ['head_vertical', 'head_horizontal']
         smach.State.__init__(self, outcomes=['success'], input_keys=input_k)
         self.wait = wait
         self.headcontrol = controller
         rospy.sleep(1)
+
     def execute(self, userdata):
         if self.hv or self.hh:
             result = self.headcontrol.set_head(self.hv, self.hh)
