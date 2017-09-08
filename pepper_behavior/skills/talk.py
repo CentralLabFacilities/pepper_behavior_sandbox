54cc44d9d594a5df4837fc6e15779a4afb58780e
Nils Neumann <nneumann@techfak.uni-bielefeld.de>
2017-09-08 12:56:18 +0200

    dreher gefixt

--- pepper_behavior/skills/talk.py
+++ pepper_behavior/skills/talk.py
@@ -9,17 +9,17 @@ class Talk(smach.State):
         self.text = text
         if  self.textblock == 'greeting':
             self.text = ['Herzlich Willkommen, mein Name ist Pepper ich bin ein Roboter und mit mir wird zur Zeit am ceitaek der Universitaet Bielefeld geforscht.',
-                     'Herzlich willkommen auf der \\readmode=word\\eitellijnz\\readmoed=sent\\ woerld 2017! Mein Name ist Pepper.',
+                     'Herzlich willkommen auf der \\readmode=word\\eitellijnz\\readmode=sent\\ woerld 2017! Mein Name ist Pepper.',
                      'Hallo, ich bin Pepper! Ich unterstuetze derzeit die Forschung am ceitaek der Universitaet Bielefeld und freue mich bei der \\readmode=word\\eitellijnz\\readmoed=sent\\ woerld 2017 dabei sein zu duerfen.',
                      'Herzlich Willkommen, mein Name ist Pepper ich bin ein Roboter und mit mir wird zur Zeit am ceitaek der Universitaet Bielefeld geforscht.']
         elif self.textblock == 'answer':
             self.text = [
-                'Mir geht es sehr gut. Ich freue mich heute auf der \\readmode=word\\eitellijnz\\readmoed=sent\\ woerld dabei sein zu duerfen.',
+                'Mir geht es sehr gut. Ich freue mich heute auf der \\readmode=word\\eitellijnz\\readmode=sent\\ woerld dabei sein zu duerfen.',
                 'Ich wurde von Softbank Robotics entwickelt und bin darauf trainiert in unterschiedlichen '
                 'Mensch-Maschine Szenarien zu unterstuetzen. Ich arbeite zurzeit am ceitaek der Universitaet ',
                 'Ich bin ein humanoider Roboter, der auf die Interaktion mit Menschen spezialisiert ist. Ich '
                 'erkenne Emotionen und kann auf die Emotionen von Menschen reagieren.',
-                'Wir zeigen Ihnen auf der \\readmode=word\\eitellijnz\\readmoed=sent\\ woerld, wie smarte, innovative Loesungen Ihr Unternehmen fuer '
+                'Wir zeigen Ihnen auf der \\readmode=word\\eitellijnz\\readmode=sent\\ woerld, wie smarte, innovative Loesungen Ihr Unternehmen fuer '
                 'die digitale Zukunft vorbereiten.']
         else:
             print('use second textblock')
