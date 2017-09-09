a6d2016fffc227d01a9dae4744f02db6fe4c4de4
llach <llach@techfak.uni-bielefeld.de>
2017-09-09 14:22:34 +0200

    change arm speed2

--- pepper_behavior/actuators/arm_control.py
+++ pepper_behavior/actuators/arm_control.py
@@ -16,6 +16,6 @@ class LeftArmControlPepper:
                                       math.radians(31.1)]
         else:
             return 'unknown_gesture'
-        self.position.speed = 0.5
+        self.position.speed = 0.35
         self.arm_pub.publish(self.position)
         return 'success'
