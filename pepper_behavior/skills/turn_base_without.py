903a383682663358e280fa390bb82b3078e912a1
llach <llach@techfak.uni-bielefeld.de>
2017-09-09 14:42:55 +0200

    add turn without

---
+++ pepper_behavior/skills/turn_base_without.py
@@ -0,0 +1,19 @@
+import smach
+import rospy
+import actionlib
+import tf
+import math
+
+class TurnWithoutMovebase(smach.State):
+    def __init__(self, controller, angle):
+	self.controller = controller
+	self.angle = angle
+        smach.State.__init__(self, outcomes=['success'])
+
+    def execute(self, userdata):
+	msg = '0:0:' + str(math.radians(angle))
+	self.controller.publish(msg)
+        return 'success'
+
+
+
