1a21e2ece201c6244a6649be2392c1053c0d4867
Felix Friese <ffriese@techfak.uni-bielefeld.de>
2017-12-01 00:10:01 +0100

    fixed transitions in AR-Demo Statemachine

--- pepper_behavior/flexbe_behaviors/src/flexbe_behaviors/pepper_ar_demo_sm.py
+++ pepper_behavior/flexbe_behaviors/src/flexbe_behaviors/pepper_ar_demo_sm.py
@@ -48,7 +48,7 @@ class PepperARDemoSM(Behavior):
 
 
 	def create(self):
-		# x:1090 y:346, x:732 y:202
+		# x:911 y:205, x:531 y:152
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.True = True
 		_state_machine.userdata.False = False
@@ -60,7 +60,7 @@ class PepperARDemoSM(Behavior):
 
 
 		with _state_machine:
-			# x:28 y:334
+			# x:41 y:306
 			OperatableStateMachine.add('WaitForHololensGoal',
 										WaitForRosNavgoalState(topic='/hololens/navgoal'),
 										transitions={'done': 'DriveToHololensGoal'},
@@ -74,48 +74,48 @@ class PepperARDemoSM(Behavior):
 										autonomy={'arrived': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'waypoint': 'navgoal'})
 
-			# x:324 y:189
+			# x:285 y:144
 			OperatableStateMachine.add('SayWayBlocked',
 										TalkState(message='My way is blocked. Please clear the way and call me again', blocking=True, force_real_robot=self.ForceRealRobot),
 										transitions={'done': 'WaitForWayClear', 'failed': 'failed'},
 										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off})
 
-			# x:645 y:56
+			# x:501 y:58
 			OperatableStateMachine.add('GraspSpaceOn',
 										PublisherBoolState(topic='/pepper_robot/hololens/grasp_space'),
 										transitions={'done': 'WaitForObject'},
 										autonomy={'done': Autonomy.Off},
 										remapping={'value': 'True'})
 
-			# x:1042 y:56
+			# x:730 y:192
 			OperatableStateMachine.add('GraspSpaceOff',
 										PublisherBoolState(topic='/pepper_robot/hololens/grasp_space'),
 										transitions={'done': 'GRASP'},
 										autonomy={'done': Autonomy.Off},
 										remapping={'value': 'False'})
 
-			# x:335 y:56
+			# x:289 y:58
 			OperatableStateMachine.add('SayHereIAm',
 										TalkState(message='Here I am, please place the object in the highlighted space', blocking=True, force_real_robot=self.ForceRealRobot),
 										transitions={'done': 'GraspSpaceOn', 'failed': 'failed'},
 										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off})
 
-			# x:308 y:332
+			# x:277 y:253
 			OperatableStateMachine.add('WaitForWayClear',
 										WaitForNaoQiSpeechState(strings_to_rec=['The way is clear now'], outcomes=['clear'], topic='/pepper_robot/speechrec/context'),
-										transitions={'clear': 'WaitForHololensGoal'},
+										transitions={'clear': 'DriveToHololensGoal'},
 										autonomy={'clear': Autonomy.Off})
 
-			# x:827 y:53
+			# x:708 y:58
 			OperatableStateMachine.add('WaitForObject',
 										WaitForNaoQiSpeechState(strings_to_rec=['There you go'], outcomes=['done'], topic='/pepper_robot/speechrec/context'),
 										transitions={'done': 'GraspSpaceOff'},
 										autonomy={'done': Autonomy.Off})
 
-			# x:1036 y:182
+			# x:725 y:311
 			OperatableStateMachine.add('GRASP',
 										TalkState(message='I would be grasping now, if I could', blocking=True, force_real_robot=self.ForceRealRobot),
-										transitions={'done': 'finished', 'failed': 'failed'},
+										transitions={'done': 'WaitForHololensGoal', 'failed': 'failed'},
 										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off})
 
 
