1a62f4b0ba106157c0c19dd04fb301981fa8b4c9
Felix Friese <ffriese@techfak.uni-bielefeld.de>
2018-01-23 00:26:56 +0100

    cosmetic changes to behavior

--- pepper_behavior/flexbe_behaviors/src/flexbe_behaviors/ar_flex_demo_sm.py
+++ pepper_behavior/flexbe_behaviors/src/flexbe_behaviors/ar_flex_demo_sm.py
@@ -48,7 +48,7 @@ class AR_Flex_DemoSM(Behavior):
 
 
     def create(self):
-        # x:417 y:77, x:421 y:138
+        # x:486 y:179, x:68 y:193
         _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 
         # Additional creation code can be added inside the following tags
@@ -56,31 +56,31 @@ class AR_Flex_DemoSM(Behavior):
         
         # [/MANUAL_CREATE]
 
-        # x:30 y:365, x:130 y:365
+        # x:417 y:37, x:130 y:365
         _sm_movetogoal_0 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['goal'])
 
         with _sm_movetogoal_0:
-            # x:85 y:38
+            # x:88 y:28
             OperatableStateMachine.add('MoveToHololens',
                                         MoveBaseState(head_angle=25),
                                         transitions={'arrived': 'finished', 'failed': 'SayBlocked'},
                                         autonomy={'arrived': Autonomy.Off, 'failed': Autonomy.Off},
                                         remapping={'waypoint': 'goal'})
 
-            # x:312 y:29
+            # x:347 y:142
             OperatableStateMachine.add('SayBlocked',
                                         TalkState(message='The way is blocked. Please clear the way and tell me when it is clear.', blocking=True),
                                         transitions={'done': 'WaitForClear', 'failed': 'WaitForClear'},
                                         autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off})
 
-            # x:186 y:153
+            # x:80 y:247
             OperatableStateMachine.add('WaitForClear',
                                         WaitForNaoQiSpeechState(strings_to_rec=['The way is clear now'], outcomes=['clear'], topic='/pepper_robot/speechrec/context'),
                                         transitions={'clear': 'MoveToHololens'},
                                         autonomy={'clear': Autonomy.Off})
 
 
-        # x:597 y:35, x:373 y:409, x:603 y:166, x:589 y:89, x:257 y:396, x:530 y:365, x:630 y:365
+        # x:533 y:33, x:115 y:357, x:540 y:161, x:537 y:83, x:39 y:358, x:187 y:354
         _sm_interruptiblemove_1 = ConcurrencyContainer(outcomes=['finished', 'failed', 'interrupted'], input_keys=['goal'], output_keys=['hololens_gen'], conditions=[
                                         ('finished', [('MoveToGoal', 'finished')]),
                                         ('finished', [('MoveToGoal', 'failed')]),
@@ -88,14 +88,14 @@ class AR_Flex_DemoSM(Behavior):
                                         ])
 
         with _sm_interruptiblemove_1:
-            # x:160 y:48
+            # x:151 y:34
             OperatableStateMachine.add('MoveToGoal',
                                         _sm_movetogoal_0,
                                         transitions={'finished': 'finished', 'failed': 'finished'},
                                         autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
                                         remapping={'goal': 'goal'})
 
-            # x:154 y:144
+            # x:140 y:144
             OperatableStateMachine.add('WaitForHololensGoal',
                                         WaitForRosNavgoalState(topic='/hololens/navgoal'),
                                         transitions={'done': 'interrupted'},
@@ -103,7 +103,7 @@ class AR_Flex_DemoSM(Behavior):
                                         remapping={'navgoal': 'hololens_gen'})
 
 
-        # x:581 y:32, x:130 y:365, x:579 y:160, x:330 y:365, x:584 y:90, x:530 y:365
+        # x:471 y:22, x:130 y:365, x:447 y:167, x:470 y:102, x:206 y:368, x:276 y:363
         _sm_interruptibledemo_2 = ConcurrencyContainer(outcomes=['finished', 'failed', 'interrupted'], output_keys=['hololens_gen'], conditions=[
                                         ('interrupted', [('WaitForHololensGoal', 'done')]),
                                         ('finished', [('Pepper AR Demo', 'finished')]),
@@ -117,7 +117,7 @@ class AR_Flex_DemoSM(Behavior):
                                         transitions={'finished': 'finished', 'failed': 'finished'},
                                         autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
 
-            # x:93 y:156
+            # x:121 y:156
             OperatableStateMachine.add('WaitForHololensGoal',
                                         WaitForRosNavgoalState(topic='/hololens/navgoal'),
                                         transitions={'done': 'interrupted'},
@@ -127,14 +127,14 @@ class AR_Flex_DemoSM(Behavior):
 
 
         with _state_machine:
-            # x:91 y:45
+            # x:262 y:23
             OperatableStateMachine.add('InterruptibleDemo',
                                         _sm_interruptibledemo_2,
                                         transitions={'finished': 'InterruptibleDemo', 'failed': 'failed', 'interrupted': 'InterruptibleMove'},
                                         autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit, 'interrupted': Autonomy.Inherit},
                                         remapping={'hololens_gen': 'hololens_gen'})
 
-            # x:86 y:176
+            # x:260 y:281
             OperatableStateMachine.add('InterruptibleMove',
                                         _sm_interruptiblemove_1,
                                         transitions={'finished': 'InterruptibleDemo', 'failed': 'failed', 'interrupted': 'InterruptibleMove'},
